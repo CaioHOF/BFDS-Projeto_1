79f1f44c6f919ab5b6e6e0c7abae9a0edc4814c2
PuDiM6241 <pudim6241@gmail.com>
2024-09-25 20:17:08 +0000

    melhorado a base, foi colocado dados para teste, e txts foram removidos ';'

--- BFDS/main.c
+++ BFDS/main.c
@@ -4,12 +4,14 @@
 #include <locale.h>
 
 void DebugCotacoes(double bitcoin, double ethereum, double ripple);
+void DebugUser(char Nome[21], char Cpf[12],char Senha[7],double Reais,double Bitcoin,double Ethereum,double Ripple);
 
 typedef struct Client
 {
 
-    char Cpf[11];
-    char Senha[20];
+    char Nome[21];
+    char Cpf[12];
+    char Senha[7];
 
     double Reais;
     double Bitcoin;
@@ -35,45 +37,53 @@ int main(int argc, char *argv[])
     FILE *pTxtCotacoes, *pTxtUsers;
     const char *Cotacoes = "Cotacoes.txt", *Users = "Users.txt";
 
-    char UserCpf[11] = "";
-    char UserSenha[20] = "";
-    char Leitor[255];
+    char userCpf[12] = "";
+    char userSenha[7] = "";
+    char leitor[256];
+
+    int i;
+    int index;
 	
-    Moeda Bitcoin;
-    Moeda Ethereum;
-    Moeda Ripple;
+    Moeda bitcoin;
+    Moeda ethereum;
+    Moeda ripple;
     
-    Bitcoin.TaxaCompra = 0.02;
-    Bitcoin.TaxaVenda = 0.03;
-    Ethereum.TaxaCompra = 0.01;
-    Ethereum.TaxaVenda = 0.02;
-    Ripple.TaxaCompra = 0.01;
-    Ripple.TaxaVenda = 0.01;
+    bitcoin.TaxaCompra = 0.02;
+    bitcoin.TaxaVenda = 0.03;
+    ethereum.TaxaCompra = 0.01;
+    ethereum.TaxaVenda = 0.02;
+    ripple.TaxaCompra = 0.01;
+    ripple.TaxaVenda = 0.01;
 
     Cpointer pClients;
     pClients = (Cpointer)calloc(10, sizeof(Client));
     if(pClients == NULL) return 1;
 
+    //isso aqui serve para ter um valor teste coerente com os dados armazenados para usar enquanto não se tem a funcao de pegar os dados
+    strcpy(pClients[0].Nome, "Josias");
+    strcpy(pClients[0].Cpf, "00000000011");
+    strcpy(pClients[0].Senha, "000001");
+    pClients[0].Reais = 0;
+    pClients[0].Bitcoin = 0;
+    pClients[0].Ethereum = 0;
+    pClients[0].Ripple = 0;
+
+
     pTxtCotacoes = fopen(Cotacoes, "r");
     if(pTxtCotacoes == NULL) return 2;
-    int index = 0;
-    while(!feof(pTxtCotacoes)){
-        fgets(Leitor, sizeof(Leitor), pTxtCotacoes);
-        index ? sscanf(Leitor, "%lf,%lf,%lf", &Bitcoin.Valor, &Ethereum.Valor, &Ripple.Valor) : index++;
-    }
+    fgets(leitor, sizeof(leitor), pTxtCotacoes);
+    fgets(leitor, sizeof(leitor), pTxtCotacoes);
+    sscanf(leitor, "%lf,%lf,%lf", &bitcoin.Valor, &ethereum.Valor, &ripple.Valor);
     fclose(pTxtCotacoes);
-    DebugCotacoes(Bitcoin.Valor, Ethereum.Valor, Ripple.Valor);
+    DebugCotacoes(bitcoin.Valor, ethereum.Valor, ripple.Valor);
     
     pTxtUsers = fopen(Users, "r");
     if(pTxtUsers == NULL) return 3;
     index = 0;
-    while (!feof(pTxtCotacoes)){
-        fgets(Leitor, sizeof(Leitor), pTxtUsers);
-        //if(index) sscanf(Leitor, "%s,%s,%lf,%lf,%lf,%lf", pClients[index-1] &Bitcoin.Valor, &Ethereum.Valor, &Ripple.Valor);
+    fgets(leitor, sizeof(leitor), pTxtUsers);
+    while (fgets(leitor, sizeof(leitor), pTxtUsers)){
         index++;
     }
-    
-
     fclose(pTxtUsers);
 
     system("pause");	
@@ -88,7 +98,14 @@ void DebugCotacoes(double bitcoin, double ethereum, double ripple){
     printf("Valores das criptos:\n\n   Bitcoin,  Ethereum,    Ripple;\n%10.2lf,%10.2lf,%10.2lf;\n", bitcoin, ethereum, ripple);
 }
 
-void DebugUser(char Cpf[11],char Senha[20],double Reais,double Bitcoin,double Ethereum,double Ripple){
-    printf("Valores do user:\n\n                 Cpf,               Senha,               Reais,             Bitcoin,            Ethereum,              Ripple;\n%20s,%20s,%20.2lf,%20.2lf,%20.2lf,%20.2lf", Cpf, Senha, Reais, Bitcoin, Ethereum, Ripple);
+void DebugUser(char Nome[21], char Cpf[12],char Senha[7],double Reais,double Bitcoin,double Ethereum,double Ripple){
+    printf("| nome: |%20s| |,| Cpf: |%11s| |,| Senha: |%6s| |,| Reais: |%.2lf| |,| Bitcoin: |%.2lf| |,| Ethereum: |%.2lf| |,| Ripple: |%.2lf| |;\n\n", Nome, Cpf, Senha, Reais, Bitcoin, Ethereum, Ripple);
 }
 
+void SaveCotacoes(){
+
+}
+
+void SaveUser(){
+    
+}
